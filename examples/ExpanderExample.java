46fe551f38132893d9b34744af99aa9deac3b647
cyrilou242 <cdecatheu@hey.com>
2025-08-05 18:44:07 +0200

    add animation to Expander

--- examples/ExpanderExample.java
+++ examples/ExpanderExample.java
@@ -8,6 +8,6 @@ public class ExpanderExample {
     public static void main(String[] args) throws InterruptedException {
         final var expanderC = Jt.expander("the-exaplanation", "See explanation").use();
         Jt.title("THE EXPLANATION TITLE").use(expanderC);
-        Jt.text("Ita fac, no way: Vindica te tibi, et tempus").use(expanderC);
+        Jt.text("Ita fac, no way: Vindica te tibi, et tempus. Lorem ipsum.").use(expanderC);
     }
 }
