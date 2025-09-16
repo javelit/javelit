34e6d01fb2d18fe57f7304d628df93fb40d945c1
cyrilou242 <cdecatheu@hey.com>
2025-09-16 22:52:35 +0200

    prep for DateInputComponent and java date parsing to jackson

--- src/main/java/tech/catheu/jeamlit/core/Shared.java
+++ src/main/java/tech/catheu/jeamlit/core/Shared.java
@@ -16,10 +16,17 @@
 package tech.catheu.jeamlit.core;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public final class Shared {
 
     public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    static {
+        OBJECT_MAPPER.registerModule(new JavaTimeModule());
+        OBJECT_MAPPER.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
 
     private Shared() {
     }
