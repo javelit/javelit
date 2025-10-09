810ccfc9d9b95341fa1bb9d4e6d3ea9cfb6c16fd
cyrilou242 <cdecatheu@hey.com>
2025-10-09 23:07:02 +0200

    add form example

---
+++ examples/form/FormExample.java
@@ -0,0 +1,31 @@
+import io.jeamlit.core.Jt;
+
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+//DEPS io.jeamlit:jeamlit:0.32.0
+
+public class FormExample {
+
+    public static void main(String[] args) {
+
+        var cols = Jt.columns("cols", 2).use();
+        Jt.empty("spacer").height(15).border(false).use(cols.col(0));
+        Jt.markdown("## Outside a form").use(cols.col(0));
+        var textInput = Jt.textInput("Outside text input").use(cols.col(0));
+        var textArea =  Jt.textArea("Outside text area").use(cols.col(0));
+        Jt.empty("spacer2").height(35).border(false).use(cols.col(0));
+        Jt.divider("divider").use(cols.col(0));
+        Jt.markdown("## Values from outside").use(cols.col(0));
+        Jt.markdown("Text input: " + textInput).use(cols.col(0));
+        Jt.markdown("Text area: " + textArea).use(cols.col(0));
+
+        var formContainer = Jt.form("my_form").use(cols.col(1));
+        Jt.markdown("## Inside a form").use(formContainer);
+        var textInputFromForm = Jt.textInput("Inside text input").use(formContainer);
+        var textAreaFromForm = Jt.textArea("Inside text area").use(formContainer);
+        Jt.formSubmitButton("Submit form").use(formContainer);
+        Jt.markdown("## Values from inside").use(cols.col(1));
+        Jt.markdown("Text input: " + textInputFromForm).key("1").use(cols.col(1));
+        Jt.markdown("Text area: " + textAreaFromForm).key("2").use(cols.col(1));
+    }
+}
