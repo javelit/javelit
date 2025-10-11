d001c00d7d82031bb17264179a222eadd45d17b5
cyrilou242 <cdecatheu@hey.com>
2025-10-11 13:25:49 +0200

    improve widget presistence example

--- examples/WidgetPersistenceAfterNoRender.java
+++ examples/WidgetPersistenceAfterNoRender.java
@@ -10,10 +10,10 @@ public class WidgetPersistenceAfterNoRender {
     public static void main(String[] args) {
         var view = Jt.radio("View", List.of("view1", "view2")).use();
         if ("view1".equals(view)) {
-            Jt.textInput("Persisted text").key("text1").use();
-            Jt.textInput("Not persisted text because no user key").use();
-            Jt.textInput("Not persisted text because noPersist").key("text3").noPersist().use();
-            Jt.text("☝️ Enter some text, then click on view2 above").use();
+            Jt.text("☝️ Enter some text in the 3 inputs, then click on view2 above").use();
+            Jt.textInput("Not persisted text because no key is provided:  \n`Jt.textInput(...).use();`").use();
+            Jt.textInput("Persisted text because a key is provided:  \n`Jt.textInput(...).key(\"text1\").use();`").key("text1").use();
+            Jt.textInput("Not persisted text because a key is provided with noPersist:  \n`Jt.textInput(...).key(\"text3\").noPersist().use();`").key("text3").noPersist().use();
         } else if ("view2".equals(view)) {
             Jt.text("☝️ Now go back to view1 and see if your text is still there").use();
         }
