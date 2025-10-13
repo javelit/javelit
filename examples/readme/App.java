59320b787a7c9902de599738fde78da1749f2e1c
cyrilou242 <cdecatheu@hey.com>
2025-10-13 22:32:54 +0200

    add readme example

---
+++ examples/readme/App.java
@@ -0,0 +1,14 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS io.jeamlit:jeamlit:0.40.0
+
+import io.jeamlit.core.Jt;
+
+public class App {
+
+    public static void main(String[] args) {
+        double size = Jt.slider("How tall are you ? in cm").max(220).use();
+        if (size > 195) {
+            Jt.text("Damn, that huge!").use();
+        }
+    }
+}
