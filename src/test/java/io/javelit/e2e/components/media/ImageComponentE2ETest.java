134eb17ada99d6dfc2d9f61592dcd0e03722ccc7
cyrilou242 <cdecatheu@hey.com>
2025-10-29 18:58:36 +0100

    simplify image test

--- src/test/java/io/javelit/e2e/components/media/ImageComponentE2ETest.java
+++ src/test/java/io/javelit/e2e/components/media/ImageComponentE2ETest.java
@@ -116,62 +116,16 @@ public class ImageComponentE2ETest {
     void testGeneratedBytes(TestInfo testInfo) {
         final @Language("java") String app = """
             import io.javelit.core.Jt;
-            import java.awt.*;
-            import java.awt.image.BufferedImage;
-            import java.io.ByteArrayOutputStream;
+            import java.nio.file.Files;
+            import java.nio.file.Path;
             import javax.imageio.ImageIO;
 
             public class TestApp {
                 public static void main(String[] args) throws Exception {
-                    byte[] imageBytes = generateHexagonImage();
+                    byte[] imageBytes = Files.readAllBytes(Path.of("examples/image/mountains.jpg"));
                     Jt.image(imageBytes)
                         .caption("Programmatically generated hexagon (800x400)")
                         .use();
-                }
-
-                private static byte[] generateHexagonImage() throws Exception {
-                    int width = 800;
-                    int height = 400;
-
-                    BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-                    Graphics2D g2d = image.createGraphics();
-
-                    // Enable anti-aliasing for smoother edges
-                    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-                    // Light blue background
-                    g2d.setColor(new Color(240, 248, 255));
-                    g2d.fillRect(0, 0, width, height);
-
-                    // Draw hexagon
-                    int centerX = width / 2;
-                    int centerY = height / 2;
-                    int radius = 150;
-
-                    int[] xPoints = new int[6];
-                    int[] yPoints = new int[6];
-
-                    for (int i = 0; i < 6; i++) {
-                        double angle = Math.PI / 3 * i - Math.PI / 6;
-                        xPoints[i] = centerX + (int) (radius * Math.cos(angle));
-                        yPoints[i] = centerY + (int) (radius * Math.sin(angle));
-                    }
-
-                    // Fill hexagon with teal color
-                    g2d.setColor(new Color(32, 178, 170));
-                    g2d.fillPolygon(xPoints, yPoints, 6);
-
-                    // Draw hexagon border
-                    g2d.setColor(new Color(0, 128, 128));
-                    g2d.setStroke(new BasicStroke(3));
-                    g2d.drawPolygon(xPoints, yPoints, 6);
-
-                    g2d.dispose();
-
-                    // Convert to PNG bytes
-                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                    ImageIO.write(image, "PNG", baos);
-                    return baos.toByteArray();
                 }
             }
             """;
