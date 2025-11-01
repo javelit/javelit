e7868f3a9c974709b3638f3fde5c8f551de1f745
javelit-ci <javelit-ci@users.noreply.github.com>
2025-11-01 03:39:48 +0000

    chore: update version references to 0.60.0

--- examples/jbang_only/App.java
+++ examples/jbang_only/App.java
@@ -1,6 +1,6 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 
-//DEPS io.javelit:javelit:0.58.0
+//DEPS io.javelit:javelit:0.60.0
 //DEPS ch.qos.logback:logback-classic:1.5.19
 
 import io.javelit.core.Jt;
